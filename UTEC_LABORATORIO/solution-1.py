d0a2262c51a22a9bb5f40a346f07b08b81f8eefe
griuswdd <victorandresdg2008@gmail.com>
2026-09-14 17:02:38 -0500

    archivos nuevos?

---
+++ UTEC_LABORATORIO/solution-1.py
@@ -0,0 +1,14 @@
+def pregunta_1(nivel_inicial_cm: int, aumento_diario_cm: int, dias: int, zonas: int) -> int:
+    return None
+
+
+def pregunta_2(nivel_inicial_cm: int, aumento_diario_cm: int, dias: int) -> int:
+    return None
+
+
+def pregunta_3(personas: int, capacidad_bote: int, nivel_rio_cm: int) -> int:
+    return None
+
+
+def pregunta_4(dias: int, nivel_inicial_cm: int, lluvia_diaria_mm: int) -> int:
+    return None
